dae90350c891f7287db3f90e438d8994915c059f
H_ritvik - D_adhich <69057115+Hritvik16000@users.noreply.github.com>
2022-11-03 00:49:57 +0530

    Add files via upload

---
+++ src/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1 className="mt-3 text-danger">Welcome to RD's Library</h1>
+      <img className="mt-3" src="https://www.myepl.org/wp-content/uploads/2021/05/home-links-get-started.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Dashboard
